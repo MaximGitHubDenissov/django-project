ee51ac9331923b9fab22cf1d4509801129de0e20
Maxim Denissov <DM7894289@gmail.com>
2024-04-17 08:36:59 +0500

    Update commit: add admin

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
+from myapp.models import Product, Order, Client, Enrollment
 
 # Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'quantity', 'add_date', 'get_image']
+    ordering = ['-add_date']
+    search_fields = ['name']
+    search_help_text = 'Название продукта'
+
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="100", height="100"')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['client', 'date_ordered', 'total_order', 'enroll_in_order']
+
+
+admin.site.register(Client)
+
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -22,6 +22,9 @@ class Product(models.Model):
     add_date = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='images', default='no_image')
 
+    def __str__(self):
+        return self.name
+
 
 class Order(models.Model):
     client = models.ForeignKey(Client, on_delete=models.SET_DEFAULT, default='ex_client')
@@ -29,9 +32,19 @@ class Order(models.Model):
     date_ordered = models.DateTimeField(auto_now_add=True)
     total_order = models.DecimalField(max_digits=9, decimal_places=2)
 
+    def __str__(self):
+        return f' Заказ № {self.pk}'
+
+    def enroll_in_order(self):
+        enrolls = Enrollment.objects.filter(order=self.pk).all()
+        return '; '.join(f'{enroll}' for enroll in enrolls)
+
 
 class Enrollment(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.product} - {self.quantity}'
 
