60109f1d571829898c365964ec7f757b00fc96f4
Maxim Denissov <DM7894289@gmail.com>
2024-04-19 11:54:03 +0500

    pre-prod commit

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t#tcghb4@7rjcu-(59*b#8b3lt9hh!1x73ner_-n7^co02_%^6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
+    ' maximdenissov.pythonanywhere.com',
 ]
 
 # Application definition
